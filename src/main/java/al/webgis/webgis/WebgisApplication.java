d57e6f42f58823d23ef192886df0a7417e97fa22
Nikoll Dodaj <nikoll.dodaj@ikubinfo.al>
2024-08-18 14:34:28 +0200

    init

---
+++ src/main/java/al/webgis/webgis/WebgisApplication.java
@@ -0,0 +1,13 @@
+package al.webgis.webgis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebgisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebgisApplication.class, args);
+	}
+
+}
