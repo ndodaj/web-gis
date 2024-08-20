ecf48e87f917a902a797418122b2a7cc8d8c7461
Nikoll Dodaj <nikoll.dodaj@ikubinfo.al>
2024-08-20 20:49:22 +0200

    fix

--- src/main/java/al/webgis/webgis/controller/GeoServerController.java
+++ src/main/java/al/webgis/webgis/controller/GeoServerController.java
@@ -3,7 +3,14 @@ package al.webgis.webgis.controller;
 import al.webgis.webgis.model.InsertFeatureDto;
 import al.webgis.webgis.service.GeoServerService;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/geoserver")
